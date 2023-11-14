fd35cccee89aa362dbf6cf5c05c7196d75346f36
Maxim Boldarev <maxikboldarev13@yandex.ru>
2023-11-14 13:23:32 +0200

    feature/di-annotation

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.*;
@@ -7,52 +8,53 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
+@Repository
 public class PostRepository {
 
-  private static final AtomicLong postCounter = new AtomicLong(0);
-  private static final Map<Long, Post> repository = new ConcurrentHashMap<>();
+    private static final AtomicLong postCounter = new AtomicLong(0);
+    private static final Map<Long, Post> repository = new ConcurrentHashMap<>();
 
-  public List<Post> all() {
-    return new ArrayList<>(repository.values());
-  }
-
-  public Optional<Post> getById(long id) {
-
-    if (id < 1 || id > postCounter.get()) {
-      return Optional.empty();
+    public List<Post> all() {
+        return new ArrayList<>(repository.values());
     }
 
-    if (repository.containsKey(id)) {
-      return Optional.of(repository.get(id));
-    } else {
-      return Optional.empty();
-    }
-  }
+    public Optional<Post> getById(long id) {
 
-  public Post save(Post post) {
-    long id = post.getId();
+        if (id < 1 || id > postCounter.get()) {
+            return Optional.empty();
+        }
 
-    if (id < 0 || id > postCounter.get()) {
-      return null;
-    }
-
-    if (id == 0) {
-      id = postCounter.addAndGet(1);
-      post.setId(id);
-      repository.put(id, post);
-    } else {
-      repository.replace(id, post);
+        if (repository.containsKey(id)) {
+            return Optional.of(repository.get(id));
+        } else {
+            return Optional.empty();
+        }
     }
 
-    return repository.get(id);
-  }
+    public Post save(Post post) {
+        long id = post.getId();
 
-  public void removeById(long id) {
+        if (id < 0 || id > postCounter.get()) {
+            return null;
+        }
 
-    if (id <= 0 || id > postCounter.get() || !repository.containsKey(id)) {
-      return;
+        if (id == 0) {
+            id = postCounter.addAndGet(1);
+            post.setId(id);
+            repository.put(id, post);
+        } else {
+            repository.replace(id, post);
+        }
+
+        return repository.get(id);
     }
 
-    repository.remove(id);
-  }
+    public void removeById(long id) {
+
+        if (id <= 0 || id > postCounter.get() || !repository.containsKey(id)) {
+            return;
+        }
+
+        repository.remove(id);
+    }
 }
--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,59 +1,59 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
+
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private static PostController controller;
-  private static final String PATH = "/api/posts";
+    private static PostController controller;
+    private static final String GET_METHOD = "GET";
+    private static final String POST_METHOD = "POST";
+    private static final String DELETE_METHOD = "DELETE";
+    private static final String POSTS_PATH = "/api/posts";
+    private static final String POSTS_ID_PATH = POSTS_PATH + "/\\d+";
 
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals(PATH)) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals("GET") && path.matches(PATH + "\\d+")) {
-        // easy way
-        final var id = getIdFromPath(path);
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals("POST") && path.equals(PATH)) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.matches(PATH + "\\d+")) {
-        // easy way
-        final var id = getIdFromPath(path);
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();git
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    @Override
+    public void init() {
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        controller = (PostController) context.getBean("postController");
     }
-  }
-  private static long getIdFromPath(String path) {
-    return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
-  }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            if (method.equals(GET_METHOD) && path.equals(POSTS_PATH)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET_METHOD) && path.matches(POSTS_ID_PATH)) {
+                final var id = getIdFromPath(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(POST_METHOD) && path.equals(POSTS_PATH)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE_METHOD) && path.matches(POSTS_ID_PATH)) {
+                final var id = getIdFromPath(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private static long getIdFromPath(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
 }
 
